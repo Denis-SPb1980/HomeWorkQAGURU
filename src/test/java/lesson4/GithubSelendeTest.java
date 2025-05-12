8d8e0064cf1f6fd1b6f5a8fcba0f0d1ac0dc5dbd
d.mishchenko <d.mishchenko@vk.team>
2025-05-12 11:11:10 +0300

    add test lesson4

---
+++ src/test/java/lesson4/GithubSelendeTest.java
@@ -0,0 +1,42 @@
+package lesson4;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GithubSelendeTest {
+
+    String expectedCode = """
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+                
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }
+               """;
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+@Test
+    void checkSoftAssertionsHasJUnit5UsageExampleTest() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-body").shouldHave(text("Soft assertions"));
+        $$("a").findBy(text("Soft assertions")).click();
+
+        $$("div.markdown-heading").findBy(text("Using JUnit5 extend test class:")).sibling(0).shouldHave(exactText(expectedCode));
+    }
+}
