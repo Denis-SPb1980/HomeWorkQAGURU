e282a801895654f705dfbcfe1cd50831e397930b
d.mishchenko <d.mishchenko@vk.team>
2025-06-24 11:35:17 +0300

    добавлен screenshots

--- src/test/java/guru/qa/TestBase.java
+++ src/test/java/guru/qa/TestBase.java
@@ -2,7 +2,9 @@ package guru.qa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 import static io.qameta.allure.Allure.step;
@@ -17,5 +19,10 @@ public class TestBase {
             Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
             SelenideLogger.addListener("allure", new AllureSelenide());
         } );
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshots");
     }
 }
