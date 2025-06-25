5e747ead118fbd68158a8e754472d34c4342874a
d.mishchenko <d.mishchenko@vk.team>
2025-06-25 23:44:19 +0300

    добавлен пропертис

--- src/test/java/guru/qa/TestBase.java
+++ src/test/java/guru/qa/TestBase.java
@@ -16,10 +16,20 @@ public class TestBase {
     @BeforeAll
     static void setUp() {
         step("Открытие страницы", () -> {
-            Configuration.browserSize = "1920x1080";
+            String selenoidHost = System.getProperty("selenoid_host",  "selenoid.autotests.cloud");
+            String selenoidLogin = System.getProperty("selenoid_login", "user1");
+            String selenoidPassword = System.getProperty("selenoid_password", "1234");
+            String browser = System.getProperty("browser", "chrome");
+            String browserVersion = System.getProperty("browserVersion", "127.0");
+            String screenResolution = System.getProperty("screenResolution", "1920x1080");
+
+            Configuration.browserSize = screenResolution;
             Configuration.baseUrl = "https://demoqa.com";
             Configuration.pageLoadStrategy = "eager";
-            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+            Configuration.remote = String.format("https://%s:%s@%s/wd/hub", selenoidLogin, selenoidPassword, selenoidHost);
+            Configuration.browser = browser;
+            Configuration.browserVersion = browserVersion;
+
             SelenideLogger.addListener("allure", new AllureSelenide());
 
             DesiredCapabilities capabilities = new DesiredCapabilities();
